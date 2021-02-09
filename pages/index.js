d4766fd73cf7873319067aff6e7ff4557c353df0
boulhend.brahim@gmail.com <boulhend.brahim@gmail.com>
2021-02-09 22:35:06 +0100

    branch edit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Fastfeedback
+          NEXT JS
         </h1>
         <h2>Great work</h2>
         <p className={styles.description}>
