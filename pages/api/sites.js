ca623630434f5de338c2003a497013fa62b9cc74
boulhend.brahim@gmail.com <boulhend.brahim@gmail.com>
2021-02-17 23:38:40 +0100

    Inni

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -4,7 +4,6 @@ import { getUserSites } from '../../lib/db-admin';
 export default async (req, res) => {
   try {
     const { uid } = await auth.verifyIdToken(req.headers.token);
-    console.log(uid);
     const sites = await getUserSites(uid);
     res.status(200).json({ sites });
   } catch (error) {
